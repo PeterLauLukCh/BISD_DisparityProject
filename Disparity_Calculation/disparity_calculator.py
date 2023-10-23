43be4123f42d02b65bc159cb585cfbbdcfe1727b
Peter Chen <147995851+PeterLauLukCh@users.noreply.github.com>
2023-10-23 10:10:11 -0400

    Add files via upload

---
+++ Disparity_Calculation/disparity_calculator.py
@@ -0,0 +1,33 @@
+import os
+import csv
+
+current_directory = os.path.dirname(__file__)
+file_path = os.path.join(current_directory, 'Names_2010Census.csv')
+frequency = []
+race_probabilities = []
+race = ["White","Black","API","AIAN","2prace","Hispanic"]
+
+with open(file_path, 'r') as csvfile:
+    csvreader = csv.reader(csvfile)
+    next(csvreader, None)
+    
+    for row in csvreader:
+        if(row[0] == 'name'):
+            continue
+        frequency.append(int(row[2]))
+        probabilities_row = []
+        for i in range(5, 11):  
+            value = row[i]
+            if value and value != '(S)':
+                probabilities_row.append(float(value) / 100)
+            else:
+                probabilities_row.append(0)
+        race_probabilities.append(probabilities_row)
+
+results = []
+for i in range(6):
+    result = sum(frequency[j] * race_probabilities[j][i] for j in range(len(frequency)))
+    results.append(result)
+
+for i in range(6):
+    print(race[i],results[i])
